016fe5acd70fa1c4a382efcc7d9fb578a4993892
UmerSarfraz5557 <umersarfraz5557@gmail.com>
2022-12-23 12:31:34 +0500

    Update ZoomageView.java

--- zoomage/src/main/java/com/jsibbold/zoomage/ZoomageView.java
+++ zoomage/src/main/java/com/jsibbold/zoomage/ZoomageView.java
@@ -172,6 +172,33 @@ public class ZoomageView extends AppCompatImageView implements OnScaleGestureLis
         verifyScaleRange();
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        try {
+            Drawable drawable = getDrawable();
+
+            if (drawable == null) {
+                setMeasuredDimension(0, 0);
+            } else {
+                float imageSideRatio = (float)drawable.getIntrinsicWidth() / (float)drawable.getIntrinsicHeight();
+                float viewSideRatio = (float)MeasureSpec.getSize(widthMeasureSpec) / (float)MeasureSpec.getSize(heightMeasureSpec);
+                if (imageSideRatio >= viewSideRatio) {
+                    // Image is wider than the display (ratio)
+                    int width = MeasureSpec.getSize(widthMeasureSpec);
+                    int height = (int)(width / imageSideRatio);
+                    setMeasuredDimension(width, height);
+                } else {
+                    // Image is taller than the display (ratio)
+                    int height = MeasureSpec.getSize(heightMeasureSpec);
+                    int width = (int)(height * imageSideRatio);
+                    setMeasuredDimension(width, height);
+                }
+            }
+        } catch (Exception e) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
     public void setTapListener(TapListener tapListener){
         this.tapListener  = tapListener;
     }
